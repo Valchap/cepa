a0346b3baf780a7655a293b7ec9d20a9cb48f2b5
Yannis Charalambidis <yannis@charalambidis.ch>
2023-04-07 22:34:01 +0200

    fun

--- cepa_routing/src/main.rs
+++ cepa_routing/src/main.rs
@@ -17,48 +17,51 @@ fn forward_message(next_hop: String, message: String) {
     n_stream.write(&message.into_bytes()).unwrap();
 }
 
-fn get_directory(data: Arc<Mutex<NodeList>>) {
+fn timed_get_dir(data: Arc<Mutex<NodeList>>) {
     let delta = Duration::from_secs(5);
     let mut next_time = Instant::now() + delta;
     loop {
-        if let Ok(mut n_stream) = TcpStream::connect("127.0.0.1:8000") {
-            match n_stream.write("GET / HTTP/1.1\nConnection: Close\n\n".as_bytes()) {
-                Ok(_) => {
-                    let mut dir_string = String::new();
-                    match n_stream.read_to_string(&mut dir_string) {
-                        Ok(_) => {
-                            let json_txt = dir_string.split("\n").last().unwrap();
-                            let t: NodeList = serde_json::from_str(json_txt).unwrap();
-                            match data.lock() {
-                                Ok(mut d) => {
-                                    d.timestamp = t.timestamp;
-                                    d.list = t.list;
-                                }
-                                Err(_) => {
-                                    panic!("Could not access data")
-                                }
-                            }
-                        }
-                        Err(_) => {
-                            println!("hey");
-                            panic!("Can't read from tcp stream")
-                        }
-                    }
-                }
-                Err(_) => {
-                    panic!("Could not send request")
-                }
-            }
-        } else {
-            panic!("Could not connect to index")
-        };
-
+        get_dir(data.clone());
         sleep(next_time - Instant::now());
         next_time += delta;
     }
 }
 
-fn handle_connection(mut stream: TcpStream) {
+fn get_dir(data: Arc<Mutex<NodeList>>) {
+    if let Ok(mut n_stream) = TcpStream::connect("127.0.0.1:8000") {
+        match n_stream.write("GET / HTTP/1.1\nConnection: Close\n\n".as_bytes()) {
+            Ok(_) => {
+                let mut dir_string = String::new();
+                match n_stream.read_to_string(&mut dir_string) {
+                    Ok(_) => {
+                        let json_txt = dir_string.split("\n").last().unwrap();
+                        let t: NodeList = serde_json::from_str(json_txt).unwrap();
+                        match data.lock() {
+                            Ok(mut d) => {
+                                d.timestamp = t.timestamp;
+                                d.list = t.list;
+                            }
+                            Err(_) => {
+                                panic!("Could not access data")
+                            }
+                        }
+                    }
+                    Err(_) => {
+                        println!("hey");
+                        panic!("Can't read from tcp stream")
+                    }
+                }
+            }
+            Err(_) => {
+                panic!("Could not send request")
+            }
+        }
+    } else {
+        panic!("Could not connect to index")
+    };
+}
+
+fn handle_connection(mut stream: TcpStream, data: Arc<Mutex<NodeList>>) {
     // stream.write(b"connection handled").unwrap();
 
     // Read from tcp stream
@@ -81,20 +84,48 @@ fn handle_connection(mut stream: TcpStream) {
     }
 }
 
+fn print_help() {
+    println!(
+        "\x1b[1mCommands:\x1b[0m
+    \x1b[1;94mdir\x1b[0m      Print the local list of announced cepa nodes
+    \x1b[1;94mget\x1b[0m      Ask the cepa_index for the list of nodes
+    \x1b[1;94mclear\x1b[0m    Clear screen
+    \x1b[1;94mexit\x1b[0m     Exit the cepa_router process
+    \x1b[1;94mhelp\x1b[0m     Print this help"
+    );
+}
+
 // Handle user input from stdin
 fn handle_user_input(data: Arc<Mutex<NodeList>>) {
     let mut user_input: String;
     let stdin = io::stdin();
     loop {
         user_input = "".to_string();
+        print!("cepa_router # ");
+        let _ = io::stdout().flush();
         stdin.read_line(&mut user_input).unwrap();
         if !user_input.is_empty() {
             match user_input.as_str() {
                 "dir\n" => {
-                    println!("{:?}", data.lock())
+                    println!("{:#?}", data.lock().unwrap())
+                }
+                "get\n" => {
+                    get_dir(data.clone());
+                }
+                "clear\n" => {
+                    print!("\x1B[2J\x1B[1;1H");
+                }
+                "exit\n" => {
+                    std::process::exit(0);
+                }
+                "help\n" => {
+                    print_help();
                 }
                 _ => {
-                    println!("{}", user_input);
+                    if user_input != "\n" {
+                        user_input.pop();
+                        println!("  Command \x1b[1;31m{}\x1b[0m not found", user_input);
+                    }
                 }
             }
         }
@@ -102,6 +133,22 @@ fn handle_user_input(data: Arc<Mutex<NodeList>>) {
 }
 
 fn main() {
+    print!("\x1b[92m");
+    println!(
+        r"
+     ▄████▄  ▓█████  ██▓███   ▄▄▄          ██▀███  ▄▄▄█████▓ ██▀███  
+    ▒██▀ ▀█  ▓█   ▀ ▓██░  ██▒▒████▄       ▓██ ▒ ██▒▓  ██▒ ▓▒▓██ ▒ ██▒
+    ▒▓█    ▄ ▒███   ▓██░ ██▓▒▒██  ▀█▄     ▓██ ░▄█ ▒▒ ▓██░ ▒░▓██ ░▄█ ▒
+    ▒▓▓▄ ▄██▒▒▓█  ▄ ▒██▄█▓▒ ▒░██▄▄▄▄██    ▒██▀▀█▄  ░ ▓██▓ ░ ▒██▀▀█▄  
+    ▒ ▓███▀ ░░▒████▒▒██▒ ░  ░ ▓█   ▓██▒   ░██▓ ▒██▒  ▒██▒ ░ ░██▓ ▒██▒
+    ░ ░▒ ▒  ░░░ ▒░ ░▒▓▒░ ░  ░ ▒▒   ▓▒█░   ░ ▒▓ ░▒▓░  ▒ ░░   ░ ▒▓ ░▒▓░
+      ░  ▒    ░ ░  ░░▒ ░       ▒   ▒▒ ░     ░▒ ░ ▒░    ░      ░▒ ░ ▒░
+    ░           ░   ░░         ░   ▒        ░░   ░   ░        ░░   ░ 
+    ░ ░         ░  ░               ░  ░      ░                 ░     
+    ░                                                                
+        "
+    );
+    print!("\x1b[0m");
     match TcpListener::bind("0.0.0.0:55505") {
         Ok(listener) => {
             // println!("Listening on 55505");
@@ -114,7 +161,7 @@ fn main() {
             let mut data_clone = data.clone();
 
             thread::spawn(move || {
-                get_directory(data_clone);
+                timed_get_dir(data_clone);
             });
 
             data_clone = data.clone();
@@ -122,14 +169,13 @@ fn main() {
                 handle_user_input(data_clone);
             });
 
-            // for stream in listener.incoming() {
-            //     let stream = stream.unwrap();
-
-            //     // TODO Fix -> Can spawn an infinite number of threads !!!!
-            //     thread::spawn(|| {
-            //         handle_connection(stream);
-            //     });
-            // }
+            for stream in listener.incoming() {
+                let stream = stream.unwrap();
+                data_clone = data.clone();
+                thread::spawn(|| {
+                    handle_connection(stream, data_clone);
+                });
+            }
         }
         Err(_) => {
             panic!("Could not bind on port 55505");
