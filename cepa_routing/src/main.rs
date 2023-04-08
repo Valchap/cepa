e2b55fc954ca9c246f1ffdd8d8b54ae54a7f8d2c
Yannis Charalambidis <yannis@charalambidis.ch>
2023-04-08 13:31:26 +0200

    Added add capability to router

--- cepa_routing/src/main.rs
+++ cepa_routing/src/main.rs
@@ -12,8 +12,14 @@ use serde_json;
 use std::thread::sleep;
 use std::time::{Duration, Instant};
 
+const CEPA_INDEX_HOST: &str = "127.0.0.1";
+const CEPA_INDEX_PORT: &str = "8000";
+const CEPA_INDEX_PUB_KEY: &str = "keyhere";
+
+const CEPA_ROUTER_PORT: &str = "55505";
+
 fn forward_message(next_hop: String, message: String) {
-    let mut n_stream = TcpStream::connect(format!("{}:55505", next_hop)).unwrap();
+    let mut n_stream = TcpStream::connect(format!("{}:{}", next_hop, CEPA_ROUTER_PORT)).unwrap();
     n_stream.write(&message.into_bytes()).unwrap();
 }
 
@@ -27,8 +33,44 @@ fn timed_get_dir(data: Arc<Mutex<NodeList>>) {
     }
 }
 
+fn add_host(d: NodeData) {
+    if let Ok(mut n_stream) = TcpStream::connect(format!("{}:{}", CEPA_INDEX_HOST, CEPA_INDEX_PORT))
+    {
+        let req = format!(
+            "
+POST / HTTP/1.1
+Content-Length: {}
+Content-Type: application/json
+Connection: Close
+
+{{\"host\": \"{}\", \"pub_key\": \"{}\"}} 
+        ",
+            (d.host.as_bytes().len() + d.pub_key.as_bytes().len() + 27),
+            d.host,
+            d.pub_key
+        );
+        match n_stream.write(req.as_bytes()) {
+            Ok(_) => {
+                let mut buf = String::new();
+                match n_stream.read_to_string(&mut buf) {
+                    Ok(_) => {
+                        println!("{}", buf);
+                    }
+                    Err(_) => {
+                        panic!("Could not read from stream")
+                    }
+                }
+            }
+            Err(_) => {
+                panic!("Could not send request to cepa_index")
+            }
+        }
+    }
+}
+
 fn get_dir(data: Arc<Mutex<NodeList>>) {
-    if let Ok(mut n_stream) = TcpStream::connect("127.0.0.1:8000") {
+    if let Ok(mut n_stream) = TcpStream::connect(format!("{}:{}", CEPA_INDEX_HOST, CEPA_INDEX_PORT))
+    {
         match n_stream.write("GET / HTTP/1.1\nConnection: Close\n\n".as_bytes()) {
             Ok(_) => {
                 let mut dir_string = String::new();
@@ -49,7 +91,6 @@ fn get_dir(data: Arc<Mutex<NodeList>>) {
                         }
                     }
                     Err(_) => {
-                        println!("hey");
                         panic!("Can't read from tcp stream")
                     }
                 }
@@ -89,12 +130,13 @@ fn handle_connection(mut stream: TcpStream, data: Arc<Mutex<NodeList>>) {
 fn print_help() {
     println!(
         "\x1b[1mCommands:\x1b[0m
-    \x1b[1;94mdir\x1b[0m            Print the local list of announced cepa nodes
-    \x1b[1;94mget\x1b[0m            Ask the cepa_index for the list of nodes
-    \x1b[1;94msend\x1b[0m HOST MSG  Send message to host
-    \x1b[1;94mclear\x1b[0m          Clear screen
-    \x1b[1;94mexit\x1b[0m           Exit the cepa_router process
-    \x1b[1;94mhelp\x1b[0m           Print this help"
+    \x1b[1;94mdir\x1b[0m                Print the local list of announced cepa nodes
+    \x1b[1;94mget\x1b[0m                Ask the cepa_index for the list of nodes
+    \x1b[1;94msend\x1b[0m HOST MSG      Send message to host
+    \x1b[1;94madd\x1b[0m  HOST PUB_KEY  Add a node to cepa_index
+    \x1b[1;94mclear\x1b[0m              Clear screen
+    \x1b[1;94mexit\x1b[0m               Exit the cepa_router process
+    \x1b[1;94mhelp\x1b[0m               Print this help"
     );
 }
 
@@ -151,6 +193,27 @@ fn handle_user_input(data: Arc<Mutex<NodeList>>) {
                         forward_message(next_hop, message);
                     }
                 }
+                "add" => {
+                    if user_input.as_str().split_whitespace().count() != 3 {
+                        println!("Usage: add HOST PUB_KEY");
+                    } else {
+                        let host = user_input
+                            .as_str()
+                            .split_whitespace()
+                            .into_iter()
+                            .nth(1)
+                            .unwrap()
+                            .to_string();
+                        let pub_key = user_input
+                            .as_str()
+                            .split_whitespace()
+                            .into_iter()
+                            .nth(2)
+                            .unwrap()
+                            .to_string();
+                        add_host(NodeData { host, pub_key });
+                    }
+                }
                 _ => {
                     if user_input != "\n" {
                         user_input.pop();
@@ -179,7 +242,7 @@ fn main() {
         "
     );
     print!("\x1b[0m");
-    match TcpListener::bind("0.0.0.0:55505") {
+    match TcpListener::bind(format!("0.0.0.0:{}", CEPA_ROUTER_PORT)) {
         Ok(listener) => {
             // println!("Listening on 55505");
 
@@ -208,7 +271,7 @@ fn main() {
             }
         }
         Err(_) => {
-            panic!("Could not bind on port 55505");
+            panic!("Could not bind on port {}", CEPA_ROUTER_PORT);
         }
     }
 }
