95b89879cb60ebb7795243cb4064457a77ba5ae9
Yannis Charalambidis <yannis@charalambidis.ch>
2023-05-26 10:03:37 +0200

    Update demo

--- demo.sh
+++ demo.sh
@@ -17,24 +17,28 @@ tmux split-window -h -c '#{pane_current_path}'
 tmux select-pane -t 0
 tmux send-keys 'docker compose run node1' C-m
 tmux send-keys 'clear' C-m
+sleep 2
 tmux send-keys 'add 192.168.1.11' C-m
 
   # Node 2
 tmux select-pane -t 1
 tmux send-keys 'docker compose run node2' C-m
 tmux send-keys 'clear' C-m
+sleep 2
 tmux send-keys 'add 192.168.1.12' C-m
 
   # Node 3
 tmux select-pane -t 2
 tmux send-keys 'docker compose run node3' C-m
 tmux send-keys 'clear' C-m
+sleep 2
 tmux send-keys 'add 192.168.1.13' C-m
 
   # Node 4
 tmux select-pane -t 3
 tmux send-keys 'docker compose run node4' C-m
 tmux send-keys 'clear' C-m
+sleep 2 
 tmux send-keys 'add 192.168.1.14' C-m
 
 tmux select-pane -t 0
